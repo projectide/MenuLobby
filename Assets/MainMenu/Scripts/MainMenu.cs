95f2bead976e32d4fc728707e2ea7eb473f5a540
Wimok <n_wimok@kkumail.com>
2016-02-18 13:44:56 +0700

    Git Project
    My project

---
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿//Attached to Main Camera
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour {
+
+    public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
